0f28c96656a16040c7582cea5aaab19898978a49
Raito Yitsushi (Balazs Nadasdi) <yitsushi@gmail.com>
2011-06-22 20:21:21 +0200

    delete 'spam' && add some comment

--- init.php
+++ init.php
@@ -1,5 +1,7 @@
 <?php
 // Define: Where is the page source on the machine
 define('BASEDIR', dirname(__FILE__));
+// Where is the root of the testing system
 define('TEST', BASEDIR.'/test');
+// Do you want to see with rainbow the result? If yes then set NOCOLOR to false
 define('NOCOLOR', true);
--- test.php
+++ test.php
@@ -5,16 +5,16 @@ if (file_exists('init.php')) {
   require 'init.php';
 }
 
-// test base dir
+// test base dir (if init.php does not exist)
 if (!defined('TEST')) {
   define('TEST', 'test');
 }
 
 require_once TEST.'/base.php';
 
-$b = new wsTestLib\wsBase();
-$b->find_and_call_tests();
-$exit_code = $b->finish();
+$main = new wsTestLib\wsBase();
+$main->find_and_call_tests();
+$exit_code = $main->finish();
 
 // for shell: exit 1 if fail detected and exit 0 if all success
 exit($exit_code == false);
